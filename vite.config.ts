575688a76e9cd482caa0efa77b29fe753c88eaca
Kurtis Lew <27038994+kurtis-lew@users.noreply.github.com>
2023-02-11 01:14:16 -0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import motionCanvas from "@motion-canvas/vite-plugin";
+
+export default defineConfig({
+  plugins: [
+    motionCanvas({
+      project: [
+        "./src/hold_tap/hold_tap_comparison.ts",
+        "./src/hold_tap/hold_tap_interrupted.ts",
+        "./src/hold_tap/balanced/balanced_comparison.ts",
+        "./src/hold_tap/balanced/balanced_other_key_up.ts",
+        "./src/hold_tap/balanced/balanced_hold_tap_up.ts",
+        "./src/hold_tap/tap_preferred/tap_preferred_comparison.ts",
+        "./src/hold_tap/tap_preferred/tap_preferred_hold_tap_up.ts",
+        "./src/hold_tap/tap_unless_interrupted/tap_unless_interrupted.ts",
+        "./src/hold_tap/tap_unless_interrupted/tap_unless_interrupted_invoke_hold.ts",
+        "./src/tap_dance/counter.ts",
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "./output/animations",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
