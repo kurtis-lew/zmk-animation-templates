70474cb70170f88a6b4e8831d1ae663a491e59cf
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-04-25 18:55:15 -0700

    feat: Convert Key from function into new component

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,82 +1,115 @@
-import { Rect, Txt, Node } from "@motion-canvas/2d/lib/components";
-import { SignalValue } from "@motion-canvas/core/lib/signals";
-import { makeRef } from "@motion-canvas/core/lib/utils";
+import { Node, NodeProps, Rect, Txt } from "@motion-canvas/2d/lib/components";
+import { easeInOutCubic, tween } from "@motion-canvas/core/lib/tweening";
+import { initial, signal } from "@motion-canvas/2d/lib/decorators";
+import {
+  createSignal,
+  SignalValue,
+  SimpleSignal,
+} from "@motion-canvas/core/lib/signals";
+import { createRef } from "@motion-canvas/core/lib/utils";
 
 const KeySize = 200;
 const KeyRadius = 10;
 export const KeyBorderThickness = 10;
 export const KeyTravel = 40;
 
-export default function Key({
-  refs,
-  binding,
-  params,
-}: {
-  refs: {
-    group: Node;
-    body: Node;
-    fill: Rect;
-    duration: Rect;
-    shadow: Rect;
-    binding: Text;
-    params: Text;
-  };
-  binding: SignalValue<string>;
-  params: SignalValue<string>;
-}) {
-  return (
-    <Node ref={makeRef(refs, "group")} y={-KeyTravel}>
-      <Rect
-        ref={makeRef(refs, "shadow")}
-        width={KeySize}
-        height={KeySize}
-        y={KeyTravel}
-        radius={KeyRadius}
-        fill={"#000000"}
-        stroke={"#000000"}
-        lineWidth={KeyBorderThickness}
-      />
-      <Node ref={makeRef(refs, "body")}>
+export interface KeyProps extends NodeProps {
+  binding?: SignalValue<string>;
+  params?: SignalValue<string>;
+}
+
+export class Key extends Node {
+  @initial("")
+  @signal()
+  public declare readonly binding: SimpleSignal<string, this>;
+
+  @initial("")
+  @signal()
+  public declare readonly params: SimpleSignal<string, this>;
+
+  private bindingText: SignalValue<string>;
+  private paramsText: SignalValue<string>;
+  private keyPosition = createSignal(0);
+  public container = createRef<Node>();
+  public body = createRef<Rect>();
+  public fill = createRef<Rect>();
+  private bindingTextBox = createRef<Txt>();
+  private paramsTextBox = createRef<Txt>();
+  public shadow = createRef<Rect>();
+
+  public constructor(props?: KeyProps) {
+    super({ ...props });
+
+    this.bindingText = this.binding();
+    this.paramsText = createSignal(this.params());
+
+    this.add(
+      <Node ref={this.container} y={-KeyTravel}>
         <Rect
           layout
-          ref={makeRef(refs, "fill")}
+          ref={this.body}
           direction={"column-reverse"}
           width={KeySize}
           height={KeySize}
+          y={() => this.keyPosition()}
           fill={"#FFFFFF"}
-        >
-          <Rect ref={makeRef(refs, "duration")} grow={0} fill={"#FFFFFF"} />
-        </Rect>
-        <Rect
-          width={KeySize}
-          height={KeySize}
-          fill={"#FFFFFF00"}
           radius={KeyRadius}
           stroke={"#000000"}
           lineWidth={KeyBorderThickness}
-        />
-        <Txt
-          ref={makeRef(refs, "binding")}
-          text={binding}
-          fill={"#000000"}
+          clip
+        >
+          <Rect ref={this.fill} grow={0} fill={"#FFFFFF"} />
+          <Txt
+            layout={false}
+            ref={this.bindingTextBox}
+            text={this.bindingText}
+            fill={"#000000"}
+            width={KeySize}
+            padding={15}
+            fontWeight={600}
+            fontSize={32}
+            fontFamily={"sans-serif"}
+            y={-65}
+          />
+          <Txt
+            layout={false}
+            ref={this.paramsTextBox}
+            text={this.paramsText}
+            fill={"#000000"}
+            width={KeySize}
+            justifyContent={"center"}
+            fontWeight={600}
+            fontSize={32}
+            fontFamily={"sans-serif"}
+          />
+        </Rect>
+
+        <Rect
+          ref={this.shadow}
           width={KeySize}
-          padding={15}
-          fontWeight={600}
-          fontSize={32}
-          fontFamily={"sans-serif"}
-          y={-65}
-        />
-        <Txt
-          ref={makeRef(refs, "params")}
-          text={params}
+          height={KeySize}
+          y={KeyTravel}
+          radius={KeyRadius}
           fill={"#000000"}
-          width={KeySize}
-          justifyContent={"center"}
-          fontWeight={600}
-          fontSize={32}
-          fontFamily={"sans-serif"}
+          stroke={"#000000"}
+          lineWidth={KeyBorderThickness}
+          zIndex={-1}
         />
       </Node>
-    </Node>
-  );
+    );
+  }
+
+  public *press(duration: number) {
+    yield* tween(duration, (value) => {
+      const currentPos = this.body().position();
+      this.keyPosition(easeInOutCubic(value, currentPos.y, KeyTravel));
+    });
+  }
+
+  public *release(duration: number) {
+    yield* tween(duration, (value) => {
+      const currentPos = this.body().position();
+      this.keyPosition(easeInOutCubic(value, currentPos.y, 0));
+    });
+  }
 }
--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,11 +1,13 @@
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
-import { makeRefs } from "@motion-canvas/core/lib/utils";
-import Key from "../components/Key";
-import { waitFor } from "@motion-canvas/core/lib/flow";
+import { createRef } from "@motion-canvas/core/lib/utils";
+import { Key } from "../components/Key";
+import { waitFor, all } from "@motion-canvas/core/lib/flow";
 
 export default makeScene2D(function* (view) {
-  const tap = makeRefs<typeof Key>();
-  view.add(<Key refs={tap} binding={"&ht_bl"} params={"LSHIFT F"} />);
+  const tapRef = createRef<Key>();
+  view.add(<Key ref={tapRef} binding={"&ht_bl"} params={"LSHIFT F"} />);
 
-  yield* waitFor(5);
+  yield* waitFor(1);
+  yield* all(waitFor(1), tapRef().press(0.25));
+  yield* all(waitFor(1), tapRef().release(0.25));
 });
