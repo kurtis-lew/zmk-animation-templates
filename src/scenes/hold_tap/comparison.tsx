be88b6a9b12c842b5faf76971bc5a7cfc1876305
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-07-08 15:29:28 -0700

    feat: Recreate hold-tap comparison

--- src/scenes/hold_tap/comparison.tsx
+++ src/scenes/hold_tap/comparison.tsx
@@ -1,44 +1,67 @@
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
 import { createRef } from "@motion-canvas/core/lib/utils";
 import { HoldTap } from "../../components/HoldTap";
-import { Key } from "../../components/Key";
 import { Terminal } from "../../components/Terminal/Terminal";
 import { waitFor, all, chain } from "@motion-canvas/core/lib/flow";
 
 export default makeScene2D(function* (view) {
-  const holdTapRef = createRef<HoldTap>();
-  const keypressRef = createRef<Key>();
-  const terminalRef = createRef<Terminal>();
+  const tapRef = createRef<HoldTap>();
+  const holdRef = createRef<HoldTap>();
+  const terminalTapRef = createRef<Terminal>();
+  const terminalHoldRef = createRef<Terminal>();
 
   view.add(
     <HoldTap
-      ref={holdTapRef}
+      ref={tapRef}
       binding={"&ht_hp"}
       params={"LSHIFT F"}
       tapping_term={1}
-      position={[-720 + 270, -(540 - 360) - 32]}
+      position={[-1440 / 4, -(540 - 360) - 32]}
     />
   );
   view.add(
-    <Key
-      ref={keypressRef}
-      params={"J"}
-      position={[-720 + 270, 540 - 360 + 32]}
+    <HoldTap
+      ref={holdRef}
+      binding={"&ht_hp"}
+      params={"LSHIFT F"}
+      tapping_term={1}
+      position={[1440 / 4, -(540 - 360) - 32]}
     />
   );
 
-  view.add(<Terminal ref={terminalRef} position={[-180, -540]} width={900} />);
-  yield* terminalRef().newLine();
-
-  yield* all(holdTapRef().press(0.25), holdTapRef().hold(0.5));
-  yield* chain(
-    holdTapRef().release(0.2),
-    waitFor(0.3),
-    all(
-      holdTapRef().decide(),
-      holdTapRef().reset(0.25),
-      terminalRef().type(1, "f", 0.1)
-    )
+  view.add(
+    <Terminal
+      ref={terminalTapRef}
+      position={[-720, 0]}
+      width={"50%"}
+      height={"50%"}
+    />
   );
-  yield* waitFor(1);
+  yield* terminalTapRef().newLine();
+
+  view.add(
+    <Terminal
+      ref={terminalHoldRef}
+      position={[0, 0]}
+      width={"50%"}
+      height={"50%"}
+    />
+  );
+  yield* terminalTapRef().newLine();
+
+  yield* all(
+    tapRef().press(0.25),
+    chain(tapRef().hold(0.25), tapRef().release(0.2)),
+    holdRef().press(0.25),
+    holdRef().hold(1)
+  );
+  yield* all(
+    tapRef().decide(),
+    tapRef().reset(0.25),
+    terminalTapRef().type(1, "f", 0.1),
+    holdRef().decide(),
+    terminalHoldRef().modifierSHIFT().activate(0.25)
+  );
+  yield* waitFor(0.25);
+  yield* all(holdRef().reset(0.25), holdRef().release(0.25), waitFor(1));
 });
