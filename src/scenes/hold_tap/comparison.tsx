d5f59a8f264c10669033c945dfe25d8bac2fbe81
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-07-08 19:35:43 -0700

    feat: Implement hold-tap comparison in separate cards

--- src/scenes/hold_tap/comparison.tsx
+++ src/scenes/hold_tap/comparison.tsx
@@ -1,4 +1,5 @@
 import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
+import { Rect } from "@motion-canvas/2d/lib/components";
 import { createRef } from "@motion-canvas/core/lib/utils";
 import { HoldTap } from "../../components/HoldTap";
 import { Terminal } from "../../components/Terminal/Terminal";
@@ -11,43 +12,64 @@ export default makeScene2D(function* (view) {
   const terminalHoldRef = createRef<Terminal>();
 
   view.add(
-    <HoldTap
-      ref={tapRef}
-      binding={"&ht_hp"}
-      params={"LSHIFT F"}
-      tapping_term={1}
-      position={[-1440 / 4, -(540 - 360) - 32]}
-    />
-  );
-  view.add(
-    <HoldTap
-      ref={holdRef}
-      binding={"&ht_hp"}
-      params={"LSHIFT F"}
-      tapping_term={1}
-      position={[1440 / 4, -(540 - 360) - 32]}
-    />
-  );
-
-  view.add(
-    <Terminal
-      ref={terminalTapRef}
-      position={[-720, 0]}
-      width={"50%"}
-      height={"50%"}
-    />
-  );
-  yield* terminalTapRef().newLine();
-
-  view.add(
-    <Terminal
-      ref={terminalHoldRef}
-      position={[0, 0]}
-      width={"50%"}
-      height={"50%"}
-    />
+    <Rect
+      width={660}
+      height={1000}
+      radius={40}
+      position={[-20, 500]}
+      offset={[1, 1]}
+      fill={"#FFFFFFFF"}
+      clip={true}
+      shadowColor={"000000DD"}
+      shadowOffsetY={10}
+      shadowBlur={20}
+    >
+      <HoldTap
+        ref={tapRef}
+        binding={"&ht_hp"}
+        params={"LSHIFT F"}
+        tapping_term={1}
+        position={[0, -150]}
+      />
+      <Terminal
+        ref={terminalTapRef}
+        position={[-330, 100]}
+        width={660}
+        height={400}
+      />
+    </Rect>
   );
   yield* terminalTapRef().newLine();
+
+  view.add(
+    <Rect
+      width={660}
+      height={1000}
+      radius={40}
+      position={[20, 500]}
+      offset={[-1, 1]}
+      fill={"#FFFFFFFF"}
+      clip={true}
+      shadowColor={"000000DD"}
+      shadowOffsetY={10}
+      shadowBlur={20}
+    >
+      <HoldTap
+        ref={holdRef}
+        binding={"&ht_hp"}
+        params={"LSHIFT F"}
+        tapping_term={1}
+        position={[0, -150]}
+      />
+      <Terminal
+        ref={terminalHoldRef}
+        position={[-330, 100]}
+        width={660}
+        height={400}
+      />
+    </Rect>
+  );
+  yield* terminalHoldRef().newLine();
 
   yield* all(
     tapRef().press(0.25),
