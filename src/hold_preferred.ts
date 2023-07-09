b714f2a2c3f0fe7f3f679adbc8d0b86ba6fa8ff9
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-07-08 23:44:41 -0700

    feat(hold_tap/hold_preferred): Implement interrupt scene

--- src/hold_preferred.ts
+++ src/hold_preferred.ts
@@ -1,7 +1,8 @@
 import { makeProject } from "@motion-canvas/core";
 
 import comparison from "./scenes/hold_tap/hold_preferred/comparison?scene";
+import interrupt from "./scenes/hold_tap/hold_preferred/interrupt?scene";
 
 export default makeProject({
-  scenes: [comparison],
+  scenes: [comparison, interrupt],
 });
