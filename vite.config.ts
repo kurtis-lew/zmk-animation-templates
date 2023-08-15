436e6ae35aade55885d2e2814a6cf7c0320dd96a
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-08-14 20:49:12 -0700

    feat(hold_tap/balanced): Implement balanced

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import motionCanvas from "@motion-canvas/vite-plugin";
 export default defineConfig({
   plugins: [
     motionCanvas({
-      project: ["./src/hold_tap/hold_preferred.ts", "./src/hold_tap/tap_preferred.ts", "./src/hold_tap/tap_unless_interrupted.ts"],
+      project: ["./src/hold_tap/hold_preferred.ts", "./src/hold_tap/balanced.ts", "./src/hold_tap/tap_preferred.ts", "./src/hold_tap/tap_unless_interrupted.ts"],
     }),
   ],
 });
