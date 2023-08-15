962a034676c4e7fd371b761e6c77ef41b86ca2a9
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-08-14 20:03:25 -0700

    refactor(hold_tap): Separate hold_tap animation configuration files into subfolder

--- src/hold_preferred.ts
+++
@@ -1,8 +0,0 @@
-import { makeProject } from "@motion-canvas/core";
-
-import comparison from "./scenes/hold_tap/hold_preferred/comparison?scene";
-import interrupt from "./scenes/hold_tap/hold_preferred/interrupt?scene";
-
-export default makeProject({
-  scenes: [comparison, interrupt],
-});
---
+++ src/hold_tap/tap_preferred.ts
@@ -0,0 +1,8 @@
+import { makeProject } from "@motion-canvas/core";
+
+import comparison from "../scenes/hold_tap/tap_preferred/comparison?scene";
+import interrupt from "../scenes/hold_tap/tap_preferred/interrupt?scene";
+
+export default makeProject({
+  scenes: [comparison, interrupt],
+});
